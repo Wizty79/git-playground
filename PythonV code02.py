918009022f9cbef139c91a8ce2e83c996f110ceb
Wizty79 <wistikwistisen@gmail.com>
2022-02-21 14:07:29 +0000

    explanation edit

--- PythonV code02.py
+++ PythonV code02.py
@@ -8,3 +8,5 @@ def message():
 
 if __name__ == '__main__': main()
 
+#in this second version, by having a conditional statement that calls main, it then forces the interpreter to read the entire code before excecuting, 
+#this allows for a more procedial style of programming. 
