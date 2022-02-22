1d8b446f24a5b5290b1bbc2384cf92b652526b0f
Wizty79 <wistikwistisen@gmail.com>
2022-02-22 09:44:08 +0000

    notes added

--- donald_class_01.py
+++ donald_class_01.py
@@ -2,7 +2,7 @@ class Duck:
     def quack(self):
         print('Quaaack!')
 
-    def walk(self):
+    def walk(self): #first argument for a method inside a class, not a ketword, can be renamed but most devs stick to self. used to refrence the object itself, see next code V
         print('Walks like a duck.')
 
 def main():
