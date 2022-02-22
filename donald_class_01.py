49fa829dd6f280adfa0c0c9dc7c03a24b12e46a9
Wizty79 <wistikwistisen@gmail.com>
2022-02-22 13:36:27 +0000

    add notes

--- donald_class_01.py
+++ donald_class_01.py
@@ -10,5 +10,16 @@ def main():
     donald.quack()
     donald.walk()
 
-if  __name__ == '__main__': main()
+if  __name__ == '__main__': main() #with an if statement you can have the code on the same line as long as it's just oen line, normally this is bad practice, but this is the exception
+#__name__ will return the name of the current modul and '__main__' tells the code this is not an imported modul but running as the main fail.    
 
+#below the function main calls the function kitten before it's defined, this is called forward declaration which Python don't support:
+#the if statement if __name__ =='__main__': main() is a common workaround for this.    
+
+#ex.def main():
+#    kitten()
+
+#def kitten():
+#    print ('Meow.')
+    
+#if __name__ =='__main__': main()
