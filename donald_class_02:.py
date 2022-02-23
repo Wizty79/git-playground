e92ac0bc5dddd9db5c7247f24130a94a0118fbd8
Wizty79 <wistikwistisen@gmail.com>
2022-02-23 12:50:09 +0000

    edit edit edit

--- donald_class_02:.py
+++ donald_class_02:.py
@@ -3,9 +3,9 @@ class Duck:
     walking = 'Walks like a duck.'
 
 
-    def quack(self):
-        print(self.sound)
-
+    def quack(self): #self is a refrence to the object, not the class, when an object is created from the class, self will refrence that object.   
+        print(self.sound) # anything that refnreces anything defined in the class, is de-refnreced off of self, to get the obstanciated object version of it.     
+                          # the . (perion) operator is used to de-refrence the object
     def walk(self):
         print(self.walking)
 
