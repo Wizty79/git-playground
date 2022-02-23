c8eb875f77937438f181810de73c95f2a76a17ba
Wizty79 <wistikwistisen@gmail.com>
2022-02-23 09:47:05 +0000

    decoratore's galore

--- decorator.py
+++ decorator.py
@@ -8,7 +8,7 @@ def f1(f):
     print('this is after the function call')
     return f2
 
-@f1
+@f1 #shortcut, this is the decorator, followed by the function definition that's just below
 def f3():
     print('this is f3')
 
