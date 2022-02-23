2f3a68fa84846a7c687865f12892d0ba794ee662
Wizty79 <wistikwistisen@gmail.com>
2022-02-23 09:36:02 +0000

    edit

--- decorator.py
+++ decorator.py
@@ -1,13 +1,16 @@
 #Decorator
 #A form of meta programming, a type of function that returns a wrapper function
 
-def f1():
+def f1(f):
     def f2():
-    print('this is f2')
+    print('this is before the function call')
+    f()
+    print('this is after the function call')
     return f2
 
+@f1
 def f3():
     print('this is f3')
 
-
+f3()
 
