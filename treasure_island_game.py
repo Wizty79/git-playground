68b49cda07c69e1f1e0f04bf47fdc5e7b6988849
Wizty79 <wistikwistisen@gmail.com>
2022-06-01 15:24:09 +0000

    hu

--- treasure_island_game.py
+++ treasure_island_game.py
@@ -27,19 +27,19 @@ choice1 = input('You\'re at a cross road with no signs. Where do you want to go?
 if choice1 == "left":
   choice2 = input('You\'ve come to a glowing lake. There is an misty island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
   if choice2 == "wait":
-    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    choice3 = input("You arrive at the island unharmed, but naked? hu. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
     if choice3 == "red":
       print("You find a book that's cursed, you can't stop reading the book and it never ends. Game Over.")
     elif choice3 == "yellow":
       print("You found the treasure! But also a powerul spellbook! Your now an apprentice witch and more adventures awaite. You Win!")
     elif choice3 == "blue":
-      print("You enter a room of passionet wearwolfs. I'll leave the rest to your imagination, wink wink. Game Over.")
+      print("You enter a room of passionet young wearwolfs. I'll leave the rest to your imagination, wink wink. Game Over.")
     else:
-      print("You chose a door that doesn't exist. Your now under a strange spell and stuck here forever. Game Over.")
+      print("You chose a door that doesn't exist. Your now under a strange spell and stuck here forever....and naked. Game Over.")
   else:
     print("You meet a lonely merman who seduces you. You are now a merman and live in the lake with him. Game Over.")
 elif choice1 == "right":
-    print("You fell into a hole and met a beautiful drow prince. You fell in love and live in his underground kingdom for 1000 years. Game Over.")
+    print("You fell into a hole and met a beautiful drow prince. You fell in love and live in his underground kingdom for at least 1000 years. Game Over.")
 else:
     print("you met a cute satyr, fell in love and stayed in his forest world forever! You are now magical and can frolic around all day and night!")
 
