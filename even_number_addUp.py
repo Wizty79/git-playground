0c78f160a9de258e895c2dbc6c766f2a12aa4e28
Wizty79 <wistikwistisen@gmail.com>
2022-06-06 12:01:23 +0000

    edit

---
+++ even_number_addUp.py
@@ -0,0 +1,12 @@
+even_sum = 0
+
+for number in range(2, 101, 2):
+  even_sum += number
+print(even_sum)
+
+total2 = 0
+for number in range(1, 101):
+  if number % 2 == 0:
+    total2 += number
+print(total2)
+
