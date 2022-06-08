716c1d9893ad450aa7eb5ee77167bf2140f6055f
Wizty79 <wistikwistisen@gmail.com>
2022-06-08 07:46:19 +0000

    more notes

--- python_revision/functions_with_input.py
+++ python_revision/functions_with_input.py
@@ -3,10 +3,10 @@ def greet():
   print("Welcome")
   print("Feel right at home")
 
-def greet_with_name(name): #here name is a variable
+def greet_with_name(name): #here name is a variable, but it's often refered to as the parameter
   print(f"Greetings {name}")
   print(f"Welcome {name}")
   print(f"Feel right at home {name}")
 
-greet_with_name("Wisti") #here we are giving that same variable a value called "Wisti"
+greet_with_name("Wisti") #here we are giving that same variable a value called "Wisti", but it's often referd to as the argument
 
