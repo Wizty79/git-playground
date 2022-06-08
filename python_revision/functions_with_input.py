1041dc9b012aff47565a8214dfd4f5a8a762acec
Wizty79 <wistikwistisen@gmail.com>
2022-06-08 07:43:47 +0000

    python revision start

---
+++ python_revision/functions_with_input.py
@@ -0,0 +1,12 @@
+def greet():
+  print("Greetings")
+  print("Welcome")
+  print("Feel right at home")
+
+def greet_with_name(name): #here name is a variable
+  print(f"Greetings {name}")
+  print(f"Welcome {name}")
+  print(f"Feel right at home {name}")
+
+greet_with_name("Wisti") #here we are giving that same variable a value called "Wisti"
+
