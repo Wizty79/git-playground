0544275a4467175144b8e3a4b4e0342e600c65e7
Wizty79 <wistikwistisen@gmail.com>
2022-06-10 15:22:02 +0000

    edit

--- Personal_Projects/Runes_of_today.py
+++ Personal_Projects/Runes_of_today.py
@@ -5,15 +5,15 @@ def select_runes(number_of_runes):
     rune_choice = random.choice(rune_list)
     return rune_choice 
 
-rune_list = ["Fehu", "Uruz", "Thurisaz", "Ansur", "Raido", "Kauna", "Gebo", "Wunjo", "Hagalaz", "Naudir", "Isar", "Jara", "ihwar", "Pertho", "Algir", "Sowelu", "Tyr", "Berkana", "Ehwas", "Mannaz", "Laguz", "Ingwar", "Odila", "Dag", "skaebne"]
-
 print("Velkommen til dine runer for i dag")
 
 first_choide = input("Skriv 'j' for at se dine runer og 'n' for at vaelge fra: ")
 if first_choide == "j":
-    
     nr_runes = int(input(f"Hvor mange Runer wil du gerne se i dag? vaelge '1', '2', '3', '4', eller '5'\n"))
-    rune_selection = for _ in range(nr_runes.select_runes()):
+    #rune_selection = for _ in range(nr_runes.select_runes()):
+    #rune_selection = for nr_runes in range(select_runes)
+    number_of_runes = nr_runes
+    rune_selection = select_runes(number_of_runes) 
     print(rune_selection)
     
 elif print("Kan du have en god dag"):
