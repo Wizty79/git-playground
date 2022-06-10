a9d39bcbf2f3bd480bb74bef36a186ceb118ca3f
Wizty79 <wistikwistisen@gmail.com>
2022-06-10 10:04:37 +0000

    simple rune app

--- Personal_Projects/Runes_of_today.py
+++ Personal_Projects/Runes_of_today.py
@@ -10,18 +10,21 @@ print("Velkommen til dine runer for i dag")
 first_choide = input("Skriv 'j' for at se dine runer og 'n' for at vaelge fra: ")
 amount_of_runes = [1, 2, 3, 4, 5]
 if first_choide == "j":
-    second_choide = input("Hvor mange Runer wil du gerne se i dag? Vaelge '1', '2', '3', '4' eller '5'" )
-    second_choice_result = amount_of_runes.append(select_runes)
-    print(f"Dette er done runer for i dag:" + {second_choide_result})
-        
-        
-        
+    nr_runes = int(input(f"Hvor mange Runer wil du gerne se i dag?\n"))
+    runes_selected = []
+    for int in nr_runes
+    runes_selected.append(random.choice(rune_list))
+    
+    print(runes_selected)
+    random.shuffle(runes_selected)
+    print(f"Dette er done runer for i dag:" + {runes_selected})
+    
+    import random
+    rune_list = ["Fehu", "Uruz", "Thurisaz", "Ansur", "Raido", "Kauna", "Gebo", "Wunjo", "Hagalaz", "Naudir", "Isar", "Jara", "ihwar", "Pertho", "Algir", "Sowelu", "Tyr", "Berkana", "Ehwas", "Mannaz", "Laguz", "Ingwar", "Odila", "Dag"]
+    todays_selection = random.sample(rune_list, 3)
+    print(todays_selection)
+    
 #    rune.choice.append(draw_runes()) 
 #elif:
 #    is_divination_over = True
 
-    
-
-
-
-        
---
+++ Personal_Projects/Runes_of_today_three_only.py
@@ -0,0 +1,5 @@
+import random
+rune_list = ["Fehu", "Uruz", "Thurisaz", "Ansur", "Raido", "Kauna", "Gebo", "Wunjo", "Hagalaz", "Naudir", "Isar", "Jara", "ihwar", "Pertho", "Algir", "Sowelu", "Tyr", "Berkana", "Ehwas", "Mannaz", "Laguz", "Ingwar", "Odila", "Dag"]
+todays_selection = random.sample(rune_list, 3)
+print(todays_selection)
+    
