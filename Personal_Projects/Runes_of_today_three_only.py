280e9d4ad1915805da69f41082b143794c9522dd
Wizty79 <wistikwistisen@gmail.com>
2022-06-10 13:22:10 +0000

    edit

--- Personal_Projects/Runes_of_today_three_only.py
+++ Personal_Projects/Runes_of_today_three_only.py
@@ -4,4 +4,5 @@ from rune_art import The_futhark_three as rune_logo
 rune_list = ["Fehu", "Uruz", "Thurisaz", "Ansur", "Raido", "Kauna", "Gebo", "Wunjo", "Hagalaz", "Naudir", "Isar", "Jara", "ihwar", "Pertho", "Algir", "Sowelu", "Tyr", "Berkana", "Ehwas", "Mannaz", "Laguz", "Ingwar", "Odila", "Dag"]
 todays_selection = random.sample(rune_list, 3)
 print(rune_logo)
+
 print(todays_selection)
