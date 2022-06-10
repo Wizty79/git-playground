ba9315a11cf2ccd06eae0216475abdcaf8626083
Wizty79 <wistikwistisen@gmail.com>
2022-06-10 13:14:28 +0000

    edit

--- Personal_Projects/Runes_of_today_tryout.py
+++ Personal_Projects/Runes_of_today_tryout.py
@@ -5,20 +5,21 @@ print("Velkommen til dine runer for i dag")
 
 first_choide = input("Skriv 'j' for at se dine runer og 'n' for at vaelge fra: ")
 if first_choide == "j":
-
     nr_runes = int(input(f"Hvor mange Runer wil du gerne se i dag? vaelge '1', '2', '3', '4', eller '5'\n"))
-    if nr_runes == '1':
+    if nr_runes == "1":
         todays_selection_one = random.sample(rune_list, 1)
         print(todays_selection_one)
-    elif nr_runes == '2':
+    elif nr_runes == "2":
         todays_selection_two = random.sample(rune_list, 2)
         print(todays_selection_two)
-    elif nr_runes == '3':
+    elif nr_runes == "3":
         todays_selection_three = random.sample(rune_list, 3)
         print(todays_selection_three)
-    elif nr_runes == '4':
+    elif nr_runes == "4":
         todays_selection_four = random.sample(rune_list, 4)
         print(todays_selection_four)
-    elif nr_runes == '5':
+    elif nr_runes == "5":
         todays_selection_five = random.sample(rune_list, 5)
         print(todays_selection_five)
+elif first_choide == "n":
+    exit()
---
+++ Personal_Projects/rune_art.py
@@ -0,0 +1,83 @@
+The_futhark_one = """
+|    |\   |      /|  |\  |/
+|/   | |  |>     /|  |/  |
+|/   |    |       |  |\  |
+fa   ur   thorn  os  rit kaun
+
+\|/     \|   |   |   /   /|\
+ X       |   |   |   \    |
+/|\      |\  |  /|   /    |
+hagalas  not is ar  sol  tyr
+
+|\   |\  \|/   |    |/   /\
+|<   |    |    |    |    \/
+|/   |    |   /|\  /|    /\
+bar laf  man  yr   eh   odal
+"""
+The_futhark_two = """
+    |\    Uraz             \/     Gebo           |\   Eihwaz
+    | | (Strength)         /\  (Partnership)    \|   (Defence)
+ The power is yours!      Togetherness.        Protect your rights.
+                                                \/
+    <>    Othila          |><|   Mannaz         <>    Inguz
+    /\  (Seperation)      |  |   (Self)         /\  (Fertility)
+Time needed alone.      Journal, letters.     Water, birth, kids.
+
+    |\     Ansuz           \|/     Algiz        \|    Nauthiz
+    |\   (Signals)          |   (Protection)     |` (Constraint)
+Hints missed and gotten.   We protect each ohter.  We are bored.
+
+    |\/     Perth          /|\  Teiwaz           /    Kano
+    |__  (Initiation)       |  (Warrior)         \  (Opening)
+I start something.      You are strong.       New Beginning.
+                                                  /
+      >   Jera              |>  Wunjo            |/     Fehu
+    <   (Harvest)           |   (Joy)            |  (Possesions)
+ Garden, smiles.      Bikes, food, art,etc...    Cars, books, etc...
+
+    |>    Raido            |_|   Hagalaz         |\   Laguz
+    |\  (Journey)          | | (Disruption)      |   (Flow)
+Work, hiking.           Fart, burp, smell,   Laughter, sex, walking.
+                                               ___
+   |\/|   Ehwaz            |>  Berkana        |   |      Odin
+   |  | (Movement)         |> (Growth)        |___| (The Unknowable)
+Aerobics, dance.          Plants, love.      Religion, weather, quantum,
+
+    /_  Sowelu             |     Isa           |\/|      Dagaz
+     / (Wholeness)         | (Standstill)      |/\|  (Breakthrough)
+Choices, trees.          Trying hard.         Listening, good health!
+
+    |
+    |>   Thurisaz
+    |   (Gateway)
+Attraction, kiss, touch.
+"""
+
+The_futhark_three = """
+                                        
+888d888888  88888888b.  .d88b. .d8888b  
+888P"  888  888888 "88bd8P  Y8b88K      
+888    888  888888  88888888888"Y8888b. 
+888    Y88b 888888  888Y8b.         X88 
+888     "Y88888888  888 "Y8888  88888P' 
+"""
+
+The_futhark_four = """
+ _ __ _   _ _ __   ___  ___ 
+| '__| | | | '_ \ / _ \/ __|
+| |  | |_| | | | |  __/\__ \
+|_|   \__,_|_| |_|\___||___/
+"""
+
+The_futhark_five = """
+                                                         
+                                                         
+8b,dPPYba, 88       88 8b,dPPYba,   ,adPPYba, ,adPPYba,  
+88P'   "Y8 88       88 88P'   `"8a a8P_____88 I8[    ""  
+88         88       88 88       88 8PP"""""""  `"Y8ba,   
+88         "8a,   ,a88 88       88 "8b,   ,aa aa    ]8I  
+88          `"YbbdP'Y8 88       88  `"Ybbd8"' `"YbbdP"'  
+
+
+
+"""
