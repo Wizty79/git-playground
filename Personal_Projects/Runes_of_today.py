5adf73c15b2d8e843353930a4a6c398628b3b4b6
Wizty79 <wistikwistisen@gmail.com>
2022-06-10 09:33:45 +0000

    indention edit

--- Personal_Projects/Runes_of_today.py
+++ Personal_Projects/Runes_of_today.py
@@ -7,18 +7,18 @@ def select_runes (number_of_runes):
 
 print("Velkommen til dine runer for i dag")
 
-    first_choide = input("Skriv 'j' for at se dine runer og 'n' for at vaelge fra: ")
-        amount_of_runes = [1, 2, 3, 4, 5]
-        if first_choide == "j":
-            second_choide = input("Hvor mange Runer wil du gerne se i dag? Vaelge '1', '2', '3', '4' eller '5'" )
-                second_choide = amount_of_runes.append(select_runes)
-                print(f"Dette er done runer for i dag:" + {second_choide})
-                
-                
-                
-            rune.choice.append(draw_runes()) 
-        elif:
-            is_divination_over = True
+first_choide = input("Skriv 'j' for at se dine runer og 'n' for at vaelge fra: ")
+amount_of_runes = [1, 2, 3, 4, 5]
+if first_choide == "j":
+    second_choide = input("Hvor mange Runer wil du gerne se i dag? Vaelge '1', '2', '3', '4' eller '5'" )
+    second_choice_result = amount_of_runes.append(select_runes)
+    print(f"Dette er done runer for i dag:" + {second_choide_result})
+        
+        
+        
+#    rune.choice.append(draw_runes()) 
+#elif:
+#    is_divination_over = True
 
     
 
