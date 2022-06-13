8036677d4ad2267cdf9bda8fdde35e0a5df1d179
Wizty79 <wistikwistisen@gmail.com>
2022-06-13 10:56:18 +0000

    add logo

--- Personal_Projects/Runes_of_today_working.py
+++ Personal_Projects/Runes_of_today_working.py
@@ -1,7 +1,8 @@
 import random
+from rune_art import The_futhark_three as rune_logo
 
 rune_list = ["Fehu", "Uruz", "Thurisaz", "Ansur", "Raido", "Kauna", "Gebo", "Wunjo", "Hagalaz", "Naudir", "Isar", "Jara", "ihwar", "Pertho", "Algir", "Sowelu", "Tyr", "Berkana", "Ehwas", "Mannaz", "Laguz", "Ingwar", "Odila", "Dag", "skaebne"]
-    
+print(rune_logo)    
 print("Velkommen til dine runer for i dag")
 
 first_choice = input("Skriv 'j' for at se dine runer og 'n' for at vaelge fra: ")
