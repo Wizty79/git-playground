a39aab6a430956807555e732f7e3846e4f5fd6e5
Wizty79 <wistikwistisen@gmail.com>
2022-06-13 10:19:10 +0000

    cleanup

--- Personal_Projects/Runes_of_today.py
+++ Personal_Projects/Runes_of_today.py
@@ -1,8 +1,9 @@
 import random
 
-def select_runes(number_of_runes):
+def select_runes():
     rune_list = ["Fehu", "Uruz", "Thurisaz", "Ansur", "Raido", "Kauna", "Gebo", "Wunjo", "Hagalaz", "Naudir", "Isar", "Jara", "ihwar", "Pertho", "Algir", "Sowelu", "Tyr", "Berkana", "Ehwas", "Mannaz", "Laguz", "Ingwar", "Odila", "Dag", "skaebne"]
-    rune_choice = number_of_runes *random.choice(rune_list)
+    #rune_choice = number_of_runes *random.choice(rune_list)
+    rune_choice = random.choice(rune_list)
     return rune_choice 
 
 print("Velkommen til dine runer for i dag")
@@ -13,7 +14,8 @@ if first_choice == "j":
     #rune_selection = for _ in range(number_of_runes.select_runes()):
     #rune_selection = for nr_runes in range(select_runes)
     number_of_runes = second_choice
-    rune_selection = select_runes(number_of_runes) # maybe use the  * operator? but how!!
+    #rune_selection = select_runes(number_of_runes) # maybe use the  * operator? but how!!
+    rune_selection = number_of_runes * select_runes()
     print(rune_selection)
     
 #elif print("Kan du have en god dag")
