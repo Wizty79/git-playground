8c1011e42c13cd079e6d2856dac966231dfc7be5
Wizty79 <wistikwistisen@gmail.com>
2022-06-13 08:36:32 +0000

    edit

---
+++ python_revision/scope_examples.py
@@ -0,0 +1,21 @@
+################### Scope ####################
+
+enemies = 1
+
+def increase_enemies():
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+############### Local Scope ############
+
+def drink_potion():
+  potion_strength = 2
+  print(potion_strength)
+
+drink_potion()
+print(potion_strength) #can't access potion_strength as it's 
+                        #defined outside of the function
+
