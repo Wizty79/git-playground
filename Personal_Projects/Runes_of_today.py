f27b429dc372ae98b3efc7b50b8c9ed0250e187b
Wizty79 <wistikwistisen@gmail.com>
2022-06-13 09:00:29 +0000

    edit

--- Personal_Projects/Runes_of_today.py
+++ Personal_Projects/Runes_of_today.py
@@ -10,10 +10,10 @@ print("Velkommen til dine runer for i dag")
 first_choice = input("Skriv 'j' for at se dine runer og 'n' for at vaelge fra: ")
 if first_choice == "j":
     second_choice = int(input(f"Hvor mange Runer wil du gerne se i dag? vaelge '1', '2', '3', '4', eller '5'\n"))
-    #rune_selection = for _ in range(nr_runes.select_runes()):
+    #rune_selection = for _ in range(number_of_runes.select_runes()):
     #rune_selection = for nr_runes in range(select_runes)
     number_of_runes = second_choice
-    rune_selection = select_runes (number_of_runes) 
+    rune_selection = select_runes(number_of_runes) # maybe use the  * operator? but how!!
     print(rune_selection)
     
 #elif print("Kan du have en god dag")
