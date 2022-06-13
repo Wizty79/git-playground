926675a6c1320cf676e48df49063755e7b9b639f
Wizty79 <wistikwistisen@gmail.com>
2022-06-13 12:01:36 +0000

    edit

--- python_revision/scope_examples.py
+++ python_revision/scope_examples.py
@@ -19,3 +19,14 @@ drink_potion()
 print(potion_strength) #can't access potion_strength as it's 
                         #defined outside of the function
 
+############## global scope #################
+
+player_health = 10
+
+def drink_potion():
+  potion_strength  = 2
+  print(player_health)
+
+#here we can use the variable player_healt due to its potision
+#it's in the global scope, and not locked inside the function. 
+  
