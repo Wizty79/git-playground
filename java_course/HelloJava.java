01a5ecf842818b6ef971b7461f4a606ae3d18576
Wizty79 <wistikwistisen@gmail.com>
2022-06-14 10:52:30 +0000

    edit

--- java_course/HelloJava.java
+++ java_course/HelloJava.java
@@ -5,3 +5,9 @@ public class HelloJava {
         System.out.println("Hello Java");
     }
 }
+
+/*when you've written new code, always compile your code with the command javac
+then run the compiled code with java
+if you make a change and run the code without compiling, it will only run the old code
+always compile 
+*/
