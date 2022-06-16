a236ba5c4054f829a8aebd0646cceb2f8438d2b8
Wizty79 <wistikwistisen@gmail.com>
2022-06-16 12:03:43 +0000

    edit

--- day 16/main.py
+++ day 16/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen 
+import _tkinter
 
 #timmy = turtle.Turtle() #if we had only said import turtle instead of the above, we would have to code as seen on this line
 timmy = Turtle()
