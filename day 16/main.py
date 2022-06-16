9ccc98f907da83328fbe14aa34b3d9c25a7b8db8
Wizty79 <wistikwistisen@gmail.com>
2022-06-16 10:35:09 +0000

    edit

---
+++ day 16/main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle 
+
+#timmy = turtle.Turtle() #if we had only said import turtle instead of the above, we would have to code as seen on this line
+timmy = Turtle()
+print(timmy)
+timmy.shape()
+
+my_screen = screen()
+print(my_screen.canvheight)
+my_screen.exttonclick()
+
+
