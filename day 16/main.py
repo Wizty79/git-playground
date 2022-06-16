4d858e4900d44f5ac0da4604ce72917da0f6eee2
Wizty79 <wistikwistisen@gmail.com>
2022-06-16 10:48:33 +0000

    edit

--- day 16/main.py
+++ day 16/main.py
@@ -1,9 +1,11 @@
-from turtle import Turtle 
+from turtle import Turtle, Screen 
 
 #timmy = turtle.Turtle() #if we had only said import turtle instead of the above, we would have to code as seen on this line
 timmy = Turtle()
 print(timmy)
-timmy.shape()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
 
 my_screen = screen()
 print(my_screen.canvheight)
