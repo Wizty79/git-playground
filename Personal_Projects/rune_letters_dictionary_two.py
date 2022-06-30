553134f4b3287452e44ecb658fc8fd918ffb5e9f
Wizty79 <wistikwistisen@gmail.com>
2022-06-30 10:07:59 +0000

    edit

--- Personal_Projects/rune_letters_dictionary_two.py
+++ Personal_Projects/rune_letters_dictionary_two.py
@@ -1,217 +1,216 @@
 
 rune_letter_One = """
 
-        letter_fehu:  
-            
-            |/
-            |/ 
-            |
-            kvaeg, penge sager, seksuel lidenska
-            
-        """
+letter_fehu:  
+
+|/
+|/ 
+|
+kvaeg, penge sager, seksuel lidenska
+
+"""
 rune_letter_two = """        
-        
-        letter_uruz 
-        
-            |\
-            | |
-            |
-            ur oksen, langsom udvikling, styrker,
-            
-        """
+
+letter_uruz 
+
+|\
+| |
+|
+ur oksen, langsom udvikling, styrker,
+
+"""
 rune_letter_three = """
-        letter_thurisaz
-        
-            |
-            |>
-            |
-        
-                
-        """
+letter_thurisaz
+
+|
+|>
+|
+
+
+"""
 rune_letter_four = """
-        letter_ansur  
-        
-            |\
-            |\
-            |
-            
-        """
-        
-rune_letter_five = """
-        letter_raido  
-                
-            |\
-            |/
-            |\
-        
-        
-        """
+letter_ansur  
+
+|\
+|\
+|
+
+"""
+
+rune_letter_five = """        letter_raido  
+
+|\
+|/
+|\
+
+
+"""
 rune_letter_six = """
-        letter_kauna 
-                
-            /
-           /
-           \
-            \
-                
-        """
+letter_kauna 
+
+/
+/
+\
+\
+
+"""
 rune_letter_seven = """
-        letter_gebo 
-                
-            \ /
-             x
-            / \
-                
-        """
+letter_gebo 
+
+\ /
+x
+/ \
+
+"""
 rune_letter_eight = """
-       letter_wunjo  
-                
-            |\
-            |/
-            |
-        
-        """
+letter_wunjo  
+
+|\
+|/
+|
+
+"""
 rune_letter_nine = """
-        letter_hagalaz  
-                
-        | |
-        |\|  
-        | | 
-        
-        """
+letter_hagalaz  
+
+| |
+|\|  
+| | 
+
+"""
 rune_letter_ten = """
-        letter_naudir 
-                
+letter_naudir 
 
-         \|
-          |
-          |\
-        
-        """
+
+\|
+|
+|\
+
+"""
 rune_letter_eleven = """
-        letter_isar  
-        
-        |
-        |
-        |
+letter_isar  
 
-        
-        """
+|
+|
+|
+
+
+"""
 rune_letter_twelve = """
-        letter_jara  
-               
-          /
-         /  \
-         \   \
-          \  /
-            /
-        
-        """ 
+letter_jara  
+
+/
+/  \
+\   \
+\  /
+/
+
+""" 
 rune_letter_thirteen = """
-        letter_ihwar  
-                
+letter_ihwar  
 
-          |\
-          |
-         \|
-        
-        """
+
+|\
+|
+\|
+
+"""
 rune_letter_fourteen = """
-        letter_pertho 
-        
-            |\/
-            |
-            |/\
-                
-        """
+letter_pertho 
+
+|\/
+|
+|/\
+
+"""
 rune_letter_fiveteen = """
-        letter_algir 
-        
-            \|/
-             |
-             |
-        
-        """
+letter_algir 
+
+\|/
+|
+|
+
+"""
 rune_letter_sixteen = """
-        letter_soweku 
-                
-        /
-        \
-        /
+letter_soweku 
 
-        """
+/
+\
+/
+
+"""
 rune_letter_seventeen = """
-        letter_tyr 
-        
-            /|\
-             |
-             |
-        
-        """
-rune_letter_eighteen = """
-        letter_berkana  
-        
-            |\
-            |/
-            |\
-            |/
-        
-        """
-rune_letter_nineteen = """
-        letter_ehwas 
-            
-            |\  /|
-            | \/ |
-            |    |
-        
-        """
-rune_letter_twenty = """
-        letter_mannaz 
-            
-            |\    /|
-            | \  / |
-            |  \/  |
-            |  /\  |  
-            | /  \ |
-            |/    \|
-            |      |
-            |      |
-        
-        """
-rune_letter_twentyone = """
-        letter_laguz 
-        
-            |\
-            | \
-            |
-            |
-        
-        """
-rune_letter_twentytwo = """
-        letter_ingwar 
-        
-            /\
-            \/
+letter_tyr 
 
-        
-        """
+/|\
+|
+|
+
+"""
+rune_letter_eighteen = """
+letter_berkana  
+
+|\
+|/
+|\
+|/
+
+"""
+rune_letter_nineteen = """
+letter_ehwas 
+
+|\  /|
+| \/ |
+|    |
+
+"""
+rune_letter_twenty = """
+letter_mannaz 
+
+|\    /|
+| \  / |
+|  \/  |
+|  /\  |  
+| /  \ |
+|/    \|
+|      |
+|      |
+
+"""
+rune_letter_twentyone = """
+letter_laguz 
+
+|\
+| \
+|
+|
+
+"""
+rune_letter_twentytwo = """
+letter_ingwar 
+
+/\
+\/
+
+
+"""
 rune_letter_twentythree = """
-        letter_odila  
-        
-            /\
-            \/
-            /\
-        
-        
-        """
+letter_odila  
+
+/\
+\/
+/\
+
+
+"""
 rune_letter_twentyfour = """
-        letter_dag 
-        
-            |\    /|
-            | \  / |
-            |  \/  |
-            |  /\  |  
-            | /  \ |
-            |/    \|
-        
-        """
+letter_dag 
+
+|\    /|
+| \  / |
+|  \/  |
+|  /\  |  
+| /  \ |
+|/    \|
+
+"""
