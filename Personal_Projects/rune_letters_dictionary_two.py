036a4a55292b3299d392aa228c83dc55b62566b1
Wizty79 <wistikwistisen@gmail.com>
2022-06-30 07:36:50 +0000

    new rune dic list

---
+++ Personal_Projects/rune_letters_dictionary_two.py
@@ -0,0 +1,270 @@
+
+rune_row = [ 
+
+        "letter_fehu": { 
+            """
+            |/
+            |/ 
+            |
+            kvaeg, penge sager, seksuel lidenskab
+
+            """
+        },
+        
+        "letter_uruz": {  
+        
+            """
+
+            |\
+            | |
+            |
+            ur oksen, langsom udvikling, styrker,
+
+            """
+        },
+        
+        "letter_thurisaz": {
+        
+            """
+            |
+            |>
+            |
+            """
+        
+        },
+        
+        "letter_ansur": {  
+        
+            """
+            |\
+            |\
+            |
+            """
+        
+        },
+        
+
+        "letter_raido": {  
+        
+            """ 
+            |\
+            |/
+            |\
+            """
+        
+        },
+        
+        "letter_kauna": { 
+        
+        """
+            /
+           /
+           \
+            \
+        
+        """
+        
+        },
+
+        "letter_gebo": { 
+        
+        """
+            \ /
+             x
+            / \
+        """
+        
+        },
+
+       "letter_wunjo": {  
+        
+        """
+            |\
+            |/
+            |
+
+        """
+        },
+
+        "letter_hagalaz": {  
+        
+        """
+        | |
+        |\|  
+        | | 
+
+        """
+        },
+        
+        "letter_naudir": { 
+        
+        """
+
+         \|
+          |
+          |\
+        
+        """
+        },
+        
+        "letter_isar": {  
+        
+        """
+
+        |
+        |
+        |
+
+        """
+        },
+        
+        "letter_jara": {  
+        
+        """ 
+          /
+         /  \
+         \   \
+          \  /
+            /
+        """
+        }, 
+        
+        "letter_ihwar": {  
+        
+        """ 
+
+          |\
+          |
+         \|
+
+        """
+        },
+
+        "letter_pertho": { 
+            
+        """ 
+
+            |\/
+            |
+            |/\
+
+        """
+        },
+
+        "letter_algir": { 
+            
+        """
+
+            \|/
+             |
+             |
+
+        """
+        },
+        
+        "letter_soweku": { 
+            
+        """ 
+
+        /
+        \
+        /
+
+        """
+        }, 
+        
+        "letter_tyr": { 
+            
+        """ 
+
+            /|\
+             |
+             |
+        """
+        },
+        
+        "letter_berkana": {  
+        
+        """
+
+            |\
+            |/
+            |\
+            |/
+
+        """
+        },
+        
+        "letter_ehwas": {  
+            
+        """
+
+            |\  /|
+            | \/ |
+            |    |
+
+        """
+        },
+        
+        "letter_mannaz": { 
+            
+        """
+
+            |\    /|
+            | \  / |
+            |  \/  |
+            |  /\  |  
+            | /  \ |
+            |/    \|
+            |      |
+            |      |
+
+        """
+        },
+        "letter_laguz": { 
+            
+        """ 
+
+            |\
+            | \
+            |
+            |
+
+        """
+        },
+        
+        "letter_ingwar": { 
+        
+        """ 
+
+            /\
+            \/
+
+        """
+        },
+        
+        "letter_odila": {  
+        
+        """ 
+
+            /\
+            \/
+            /\
+        
+        """
+        }, 
+
+        "letter_dag": { 
+        
+        """
+
+            |\    /|
+            | \  / |
+            |  \/  |
+            |  /\  |  
+            | /  \ |
+            |/    \|
+
+        """
+        },
+]
